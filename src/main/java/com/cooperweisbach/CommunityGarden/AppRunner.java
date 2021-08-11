85ae510ffc01c6570f2069312d22a29e5c925b2f
cooperweisbach <cooperweisbach@gmail.com>
2021-08-11 14:54:01 -0700

    Statuses, types, and roles have been successfully added to an SQL database

--- src/main/java/com/cooperweisbach/CommunityGarden/AppRunner.java
+++ src/main/java/com/cooperweisbach/CommunityGarden/AppRunner.java
@@ -1,9 +1,7 @@
 package com.cooperweisbach.CommunityGarden;
 
 import com.cooperweisbach.CommunityGarden.daos.*;
-import com.cooperweisbach.CommunityGarden.models.LeasableStatus;
-import com.cooperweisbach.CommunityGarden.models.LeaseStatus;
-import com.cooperweisbach.CommunityGarden.models.Payment;
+import com.cooperweisbach.CommunityGarden.models.*;
 import com.cooperweisbach.CommunityGarden.services.MemberServices;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -76,8 +74,47 @@ public class AppRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        //Add statuses, types, and user roles
+        leasableTypeRepo.save(new LeasableType("outdoorGardenPlot"));
+        leasableTypeRepo.save(new LeasableType("beeHive"));
+        leasableTypeRepo.save(new LeasableType("workshop"));
+        leasableTypeRepo.save(new LeasableType("greenhousePlot"));
+        leasableTypeRepo.save(new LeasableType("fishTank"));
+        leasableStatusRepo.save(new LeasableStatus("open"));
+        leasableStatusRepo.save(new LeasableStatus("closed"));
+        leasableStatusRepo.save(new LeasableStatus("leased"));
+        leaseStatusRepo.save(new LeaseStatus("active"));
+        leaseStatusRepo.save(new LeaseStatus("expired"));
+        memberStatusRepo.save(new MemberStatus("active"));
+        memberStatusRepo.save(new MemberStatus("inactive"));
+        messageThreadStatusRepo.save(new MessageThreadStatus("open"));
+        messageThreadStatusRepo.save(new MessageThreadStatus("closed"));
+        paymentTypeRepo.save(new PaymentType("newMemberPayment"));
+        paymentTypeRepo.save(new PaymentType("leasePayment"));
+        paymentTypeRepo.save(new PaymentType("donation"));
+        postStatusRepo.save(new PostStatus("published"));
+        postStatusRepo.save(new PostStatus("inProgress"));
+        postStatusRepo.save(new PostStatus("element"));
+        userRolesRepo.save(new UserRoles("head"));
+        userRolesRepo.save(new UserRoles("admin"));
+        userRolesRepo.save(new UserRoles("adminRead"));
+        userRolesRepo.save(new UserRoles("adminReadWrite"));
+        userRolesRepo.save(new UserRoles("member"));
+        imageStatusRepo.save(new ImageStatus("defaultProfilePicture"));
+        imageStatusRepo.save(new ImageStatus("profilePicture"));
+        imageStatusRepo.save(new ImageStatus("postImage"));
+        imageStatusRepo.save(new ImageStatus("slideShowHP"));
+        imageStatusRepo.save(new ImageStatus("gallery"));
 
-        // add dummy data
+
+
+
+
+
+
+
+        //Adding members
+
 
 
     }
